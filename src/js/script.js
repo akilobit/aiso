25b7be6ae3c83958e4e93b061a1ca286cb3883d9
Ozzy <ozzy@ozzy.no>
2022-07-03 22:52:02 +0200

    forgot a file

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ const processTracks = items => {
 	const html = items.map(item => {
 		let url = 'https://soundcloud.com/aiso/' + item.url
 		let track = `<div class="track">
-			<a rel="noopener nofollow" href="${item.url}" target="_blank" aria-label="${item.title}"><img alt="${item.title}" sizes="(max-width: 500px) 500px, 300px" src="/images/cover/cover-${item.cover}-300x300.jpg" srcset="/images/cover/cover-${item.cover}.jpg 500w, /images/cover/cover-${item.cover}-300x300.jpg 300w" loading="lazy"></a>
+			<a rel="noopener nofollow" href="${url}" target="_blank" aria-label="${item.title}"><img alt="${item.title}" sizes="(max-width: 500px) 500px, 300px" src="/images/cover/cover-${item.cover}-300x300.jpg" srcset="/images/cover/cover-${item.cover}.jpg 500w, /images/cover/cover-${item.cover}-300x300.jpg 300w" loading="lazy"></a>
 			<p class="date">${item.date}</p>
 			<p class="tags">${item.tags.join(', ')}</p>
 			<h3><a rel="noopener nofollow" href="${url}" target="_blank" aria-label="${item.title}">${item.title}</a></h3>
